40735766db0a2361649eeb1f0ea66de9fc9ffe71
Ch4rlotteSun <charlottesun2021@gmail.com>
2025-03-26 17:30:31 +1300

    Create BeginnersProblemSet.py

---
+++ BeginnersProblemSet.py
@@ -0,0 +1,11 @@
+#1
+E = int(input("Input a value in minutes: "))
+print(E*60)
+
+#2
+W = int(input("How many cards did Cruella get this year? "))
+print("Jane recieves at least" ,W*2)
+
+#3
+A = int(input("How old are you(in years)? "))
+print("Your age in days is", A*365)
